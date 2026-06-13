9254bb4ab7b9f408fc6104c32be815da46f51b2e
PumpFun711 <fu.cto.mail@gmail.com>
2026-06-13 23:07:47 +0200

    Update network.js

--- network.js
+++ network.js
@@ -21,52 +21,42 @@ const Network = {
       showToast('❌ ' + data.message);
     });
 
-    // Server sends full world + players on join
     this.socket.on('init', (data) => {
       Game.onInit(data);
     });
 
-    // Another player joined
     this.socket.on('playerJoined', (data) => {
       Game.onPlayerJoined(data);
     });
 
-    // Another player moved
     this.socket.on('playerMoved', (data) => {
       Game.onPlayerMoved(data);
     });
 
-    // Another player left
     this.socket.on('playerLeft', (data) => {
       Game.onPlayerLeft(data);
     });
 
-    // Block fully broken by anyone
     this.socket.on('blockBroken', (data) => {
       Game.onBlockBroken(data);
     });
 
-    // Our hit landed but block not broken yet
     this.socket.on('blockHit', (data) => {
       Game.onBlockHit(data);
     });
 
-    // Our pickaxe was upgraded
     this.socket.on('pickaxeUpgraded', (data) => {
       Game.onPickaxeUpgraded(data);
     });
 
-    // Another player changed pickaxe
     this.socket.on('playerPickaxeChanged', (data) => {
       Game.onPlayerPickaxeChanged(data);
     });
 
-    // Leaderboard update
     this.socket.on('leaderboard', (data) => {
       HUD.updateLeaderboard(data);
     });
 
-    // Player count update
     this.socket.on('playerCount', (count) => {
       document.getElementById('hud-online').textContent = count + '/60';
     });
@@ -77,25 +67,21 @@ const Network = {
     this.socket.emit('join', playerData);
   },
 
-  // Send position update
   move(x, y, z, rotY, isWalking) {
     if (!this.socket) return;
     this.socket.emit('move', { x, y, z, rotY, isWalking });
   },
 
-  // Mine a block
   mineBlock(x, y, z) {
     if (!this.socket) return;
     this.socket.emit('mineBlock', { x, y, z });
   },
 
-  // Buy pickaxe upgrade
   buyPickaxe(tier) {
     if (!this.socket) return;
     this.socket.emit('buyPickaxe', { tier });
   },
 
-  // Request leaderboard
   getLeaderboard() {
     if (!this.socket) return;
     this.socket.emit('getLeaderboard');
